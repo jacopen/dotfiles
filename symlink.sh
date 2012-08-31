fd0a8db2f39d9c8cc622a6c6538cb85f546102fc
Kazuto Kusama <jacopen@gmail.com>
2012-08-31 19:36:21 +0900

    シンボリックリンク張るスクリプト作成

---
+++ symlink.sh
@@ -0,0 +1,7 @@
+CURRENT=`pwd` 
+ln -s $CURRENT/.vimrc ~/.vimrc
+ln -s $CURRENT/.screenrc ~/.screenrc
+ln -s $CURRENT/.zshenv ~/.zshenv
+ln -s $CURRENT/.zshrc ~/.zshrc
+ln -s $CURRENT/vimfiles ~/vimfiles
+ln -s $CURRENT/.vim ~/.vim
